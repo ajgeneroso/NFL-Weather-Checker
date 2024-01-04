f4e08d8c9afdb749af94a8fe08939763affd5e7f
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-01-04 13:18:36 -0500

    js changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,19 +1,19 @@
-var requestUrl = 'https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/athletes/14876/stats';
+var requestUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams';
 var apiUrl = 'https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=InSyQJBRzUZVQlYpMWDOxMQ5NlBmNRTT';
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Fetch Response \n-------------');
-    console.log(data.categories[0]);
+    console.log('NFL Teams \n-------------');
+    console.log(data.sports[0].leagues[0].teams);
   });
   fetch(apiUrl)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log('Fetch Response \n-------------');
+    console.log('Weather \n-------------');
     console.log(data);
   });
 
