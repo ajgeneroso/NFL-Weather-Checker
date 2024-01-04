2448820fbd03899f459a47a4023a4d752e5c3966
Paige Preziosi <paigepreziosi@Paiges-Laptop.lan>
2024-01-04 12:47:46 -0500

    weather widget

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,14 @@
 var requestUrl = 'https://site.web.api.espn.com/apis/common/v3/sports/football/nfl/athletes/14876/stats';
+var apiUrl = 'https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=InSyQJBRzUZVQlYpMWDOxMQ5NlBmNRTT';
 fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log('Fetch Response \n-------------');
+    console.log(data.categories[0]);
+  });
+  fetch(apiUrl)
   .then(function (response) {
     return response.json();
   })
@@ -7,3 +16,7 @@ fetch(requestUrl)
     console.log('Fetch Response \n-------------');
     console.log(data);
   });
+
+
+  document.querySelector(".city").innerHTML = location.name;
+
