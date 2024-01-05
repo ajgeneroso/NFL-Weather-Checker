ffc0b4890354f2b7916f6555348fa1ea3398e48a
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-01-05 14:44:40 -0500

    js changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,5 +1,13 @@
 var requestUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams';
-var apiUrl = 'https://api.tomorrow.io/v4/weather/realtime?location=toronto&apikey=InSyQJBRzUZVQlYpMWDOxMQ5NlBmNRTT';
+var apiUrl = 'https://api.tomorrow.io/v4/weather, /realtime?location=toronto&apikey=InSyQJBRzUZVQlYpMWDOxMQ5NlBmNRTT';
+var nflTeams = ["Arizona", "Atlanta", "Baltimore", "Buffalo", "Carolina", "Chicago", "Cincinnati", "Cleveland", "Dallas", "Denver", "Detroit", "Green Bay", "Houston", "Indianapolis", "Jacksonville", "Kansas City", "Las Vegas", "LAC", "LAR", "Miami", "Minnesota", "New England", "New Orleans", "NYG", "NYJ", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Tampa Bay", "Tennessee", "Washington"];
+
+var teamDiv = document.getElementById('teams');
+
+document.getElementById("myBtn").addEventListener("click", displayTeams);
+function displayTeams() {
+  // alert("Clicked!")
+
 fetch(requestUrl)
   .then(function (response) {
     return response.json();
@@ -7,15 +15,23 @@ fetch(requestUrl)
   .then(function (data) {
     console.log('NFL Teams \n-------------');
     console.log(data.sports[0].leagues[0].teams);
-  });
-  fetch(apiUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Weather \n-------------');
-    console.log(data);
-  });
+
+    for( var i = 0; i < data.sports[0].leagues[0].teams.length; i++){
+    //CREATE
+    var newTeam = document.createElement('button');
+        
+
+    //INJECT
+    newTeam.textContent = data.sports[0].leagues[0].teams[i].team.displayName;
+    newTeam.dataset.city = data.sports[0].leagues[0].teams[i].team.location;
+
+    //APPEND
+    teamDiv.appendChild(newTeam);
+    }
+    
+})
+
+  };
 
 
 
