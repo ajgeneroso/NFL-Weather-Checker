bea4875078defecdf8b8f42a77b852e0b90f04aa
Mike Neal <152903947+Mike-Neal@users.noreply.github.com>
2024-01-06 02:05:50 -0500

    js cleared console

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,63 +1,46 @@
 var requestUrl = 'https://site.api.espn.com/apis/site/v2/sports/football/nfl/teams';
-var nflTeams = ["Arizona", "Atlanta", "Baltimore", "Buffalo", "Carolina", "Chicago", "Cincinnati", "Cleveland", "Dallas", "Denver", "Detroit", "Green Bay", "Houston", "Indianapolis", "Jacksonville", "Kansas City", "Las Vegas", "LAC", "LAR", "Miami", "Minnesota", "New England", "New Orleans", "NYG", "NYJ", "Philadelphia", "Pittsburgh", "San Francisco", "Seattle", "Tampa Bay", "Tennessee", "Washington"];
-
 var teamDiv = document.getElementById('teams');
 
 document.getElementById("myBtn").addEventListener("click", displayTeams);
 function displayTeams() {
-  // alert("Clicked!")
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (var i = 0; i < data.sports[0].leagues[0].teams.length; i++) {
+        var newTeam = document.createElement('input');
 
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('NFL Teams \n-------------');
-    console.log(data.sports[0].leagues[0].teams);
+        newTeam.type = 'image';
+        newTeam.src = data.sports[0].leagues[0].teams[i].team.logos[0].href;
+        newTeam.textContent = data.sports[0].leagues[0].teams[i].team.displayName;
+        newTeam.dataset.city = data.sports[0].leagues[0].teams[i].team.location;
+        newTeam.style = "width:100px;"
 
-    for( var i = 0; i < data.sports[0].leagues[0].teams.length; i++){
-    //CREATE
-    var newTeam = document.createElement('input');
-        
-
-    //INJECT
-    newTeam.type = 'image';
-    newTeam.src = data.sports[0].leagues[0].teams[i].team.logos[0].href;
-    newTeam.textContent = data.sports[0].leagues[0].teams[i].team.displayName;
-    newTeam.dataset.city = data.sports[0].leagues[0].teams[i].team.location;
-    newTeam.style = "width:100px;"
-    //APPEND
-    teamDiv.appendChild(newTeam);
-    }
-    
-})
+        teamDiv.appendChild(newTeam);
+      }
+    })
+};
 
 
 
-  };
-
-   
 
 
+var temperature = document.querySelector('.temp');
+var city = document.querySelector('.city');
 
-   var temperature = document.querySelector('.temp');
-   var city = document.querySelector('.city');
+teamDiv.addEventListener('click', function (e) {
+  var apiUrl = `https://api.tomorrow.io/v4/weather/realtime?location=${e.target.dataset.city}&units=imperial&apikey=InSyQJBRzUZVQlYpMWDOxMQ5NlBmNRTT`;
 
-   teamDiv.addEventListener('click', function(e){
-    var apiUrl = `https://api.tomorrow.io/v4/weather/realtime?location=${e.target.dataset.city}&units=imperial&apikey=InSyQJBRzUZVQlYpMWDOxMQ5NlBmNRTT`;
-
-    fetch(apiUrl)
-     .then(function (response) {
-       return response.json();
-     })
-     .then(function (data) {
-       console.log('Weather \n-------------');
-       console.log(data);
-       temperature.textContent = data.data.values.temperature; 
-       city.textContent = data.location.name;
-       console.log(data.location.name);
-     });  
-   });
+  fetch(apiUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      temperature.textContent = data.data.values.temperature;
+      city.textContent = data.location.name;      
+    });
+});
 
 
 
